b04af235720d29a7f6f9e21fab6cedf687c2e14f
Mario Domenech Goulart <mario@ossystems.com.br>
2015-03-06 15:31:00 -0300

    Add "human readable" transformer for compiler command lines in logs
    The -H option for the `log' (l) command will format and number calls
    to gcc/g++ in logs.  The "human readable" format shows compiler
    arguments separated by newlines.
    
    Change-Id: I57838b21e0b7681b490991ef8fcd89d86af7b9e6
    Signed-off-by: Mario Domenech Goulart <mario@ossystems.com.br>

---
+++ format-gcc-calls.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import errno
+
+def show(line):
+    try:
+        sys.stdout.write(line)
+        sys.stdout.flush()
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            input.close()
+            sys.exit(0)
+
+input_file = sys.argv[1]
+input = open(input_file, 'r')
+gcc_call_counter = 0
+
+while True:
+    build_line = input.readline()
+    if not build_line:
+        break
+
+    tokens = build_line.split()
+    if len(tokens) > 0:
+        prev = None
+        first_token = tokens[0]
+
+        filename = os.path.basename(first_token)
+        if (filename.endswith('gcc') or
+            filename.endswith('g++') or
+            filename.endswith('xgcc')):
+            gcc_call_counter += 1
+            print('--------------[ GCC call %s ]----------------------' % gcc_call_counter)
+            lines = [first_token]
+            for token in tokens[1:]:
+                if prev in ['-o', '-isystem']:
+                    lines[-1] += ' ' + token
+                else:
+                    lines.append(token)
+                prev = token
+
+            show(lines[0] + '\n')
+            for line in lines[1:]:
+                show('  ' + line + '\n')
+        else:
+            show(build_line)
+input.close()
